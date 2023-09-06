b94fbc8cf998c2d882fb39c7712b0136ba5e7649
DommSite <johan.johansson06@live.se>
2023-09-06 10:31:03 +0200

    Add project files.

---
+++ övning1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uppgift1_1
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        //Här efter kommer självaste kommandot som skriver ut texten
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.\r\n" +
+                "Du har lärt dig skriva ut tecken som &quot; och \\.");//
+        }
+    }
+}
